42422ee7f6a5b6dc45c6bfd09031826726e0fc6a
sanayuj <sanayuj2255@gmail.com>
2024-02-13 17:37:42 +0530

    first commit

---
+++ EmojiLover/EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Sanay UJ on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
