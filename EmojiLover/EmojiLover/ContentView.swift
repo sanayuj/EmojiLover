2a3656e72543074bdab7c160eba236608f3e50c3
sanayuj <sanayuj2255@gmail.com>
2024-02-13 23:53:21 +0530

    hello world display

--- EmojiLover/EmojiLover/ContentView.swift
+++ EmojiLover/EmojiLover/ContentView.swift
@@ -7,15 +7,13 @@
 
 import SwiftUI
 
+enum Emoji{
+    
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello world")
     }
 }
 
