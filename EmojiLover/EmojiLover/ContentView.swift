8f46439202124815d7d3cefa077d0da9d5fa1193
sanayuj <sanayuj2255@gmail.com>
2024-02-14 11:55:32 +0530

    nav head

--- EmojiLover/EmojiLover/ContentView.swift
+++ EmojiLover/EmojiLover/ContentView.swift
@@ -14,15 +14,18 @@ enum Emoji:String,CaseIterable{
 struct ContentView: View {
     @State var value:Emoji = .🐟
     var body: some View {
-        VStack{
-            Text(value.rawValue).font(.system(size: 150))
-            Picker("Emoji selector", selection: $value) {
-                            ForEach(Emoji.allCases, id: \.self) { emoji in
-                                Text(emoji.rawValue)
-                            }
-            }.pickerStyle(.segmented)
+        NavigationView{
+            VStack{
+                Text(value.rawValue).font(.system(size: 150))
+                Picker("Emoji selector", selection: $value) {
+                    ForEach(Emoji.allCases, id: \.self) { emoji in
+                        Text(emoji.rawValue)
+                    }
+                }.pickerStyle(.segmented)
+            }.navigationTitle("Pet Selector")
+                .padding()
         }
-     
+       
     }
 }
 
